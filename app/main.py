318110aaec5b1772cee1699445207086a72bd109
Johan Gray <jgray@jyarg.fr>
2023-05-21 09:47:19 +0000

    we did it thrice

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest and we did it. We finally build a CI/CD pipeline !!"}
+    return {"Hello": "We Love Datascientest and we did it. We finally build a CI/CD pipeline !!...maybe"}
